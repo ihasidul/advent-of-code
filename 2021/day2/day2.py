757f2819af7fc45d676c67ea8ec69d30e0492399
ihasidul <ihasidul@gmail.com>
2021-12-04 00:16:59 +0600

    Add day 2 part 2

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -14,4 +14,22 @@ def get_final_position(lst):
             depth = depth - int(x[1])
     return horizontal * depth
 
-print(get_final_position(lines))
+def get_final_position_with_aim(lst):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for l in lst:
+        x = l.split()
+        if x[0] == 'forward':
+            horizontal = horizontal + int(x[1])
+            depth = depth + (aim * int(x[1]))
+        elif x[0] == 'down':
+            aim = aim + int(x[1])
+        elif x[0] == 'up':
+            aim = aim - int(x[1])
+
+    return horizontal * depth
+
+
+#print(get_final_position(lines))
+print(get_final_position_with_aim(lines))
