1557763d1db2cad28ff0a3c1e56d71d25a4dd845
ihasidul <ihasidul@gmail.com>
2021-12-03 23:26:28 +0600

    Add day one part 2 solution

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,18 +1,37 @@
-result = 0
 def is_increase(previous,current):
     if current > previous:
         return True
     else:
         return False
 
+
+def find_total_increase(lines):
+    result = 0
+    for idx, num in enumerate(lines):
+        if is_increase(lines[idx-1],lines[idx]):
+            result = result + 1 
+        else:
+            continue 
+    return result
+
 with open('input.txt') as f:
     lines = [int(line.rstrip()) for line in f]
 
-print(type(lines[0]))
+def get_sum_of_every_three_num(lines):
+    sum_list = []
+    for idx, num in enumerate(lines):
+        if idx < len(lines) - 2:
+            s = lines[idx] + lines[idx + 1] + lines[idx + 2]
+            print(s)
+            sum_list.append(s)
+    return sum_list
 
-for idx, num in enumerate(lines):
-    if is_increase(lines[idx-1],lines[idx]):
-        result = result + 1 
-    else:
-        continue 
-print(result)
+
+# print(find_total_increase(lines))
+
+l = [199,200,208,210]
+three_sum_lst = get_sum_of_every_three_num(lines)
+print(get_sum_of_every_three_num(lines))
+
+print(find_total_increase(three_sum_lst))
+
